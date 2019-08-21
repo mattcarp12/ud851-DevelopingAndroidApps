1ba537db8ac2eb67b03ed9ed9ccfa65c8f5b9d8a
mattcarp12 <mattcarp88@gmail.com>
2019-08-21 08:02:06 -0400

    complete exercise 5a.03

--- ud851-Exercises/Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ ud851-Exercises/Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mLifecycleDisplay;
 
     // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    static List<String> mLifeCycleCallbacks = new ArrayList<>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -72,9 +76,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
-
+        for (String s : mLifeCycleCallbacks) {
+            mLifecycleDisplay.append(s + "\n");
+        }
         // TODO (5) Clear mLifecycleCallbacks after iterating through it
-
+        mLifeCycleCallbacks = new ArrayList<>();
         logAndAppend(ON_CREATE);
     }
 
@@ -134,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
 
         // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
-
+        mLifeCycleCallbacks.add(ON_STOP);
         logAndAppend(ON_STOP);
     }
 
@@ -161,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
 
         // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
-
+        mLifeCycleCallbacks.add(ON_DESTROY);
         logAndAppend(ON_DESTROY);
     }
 
